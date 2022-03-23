4e0572dfdfa7108dff9394df7022c0860cedae4b
Alexander Breus <Lis00909@gmail.com>
2022-03-23 17:37:58 +0300

    Инициализация проекта

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Grid from '@mui/material/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+               
+                </Grid>
+                <Grid item xs={8}>
+                
+                </Grid>
+            </Grid>
+
+    );
 }
 
 export default App;
